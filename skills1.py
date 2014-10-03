63e5fe4d61c92b43a417d843dacc1d172c6c235c
Hackbright Student <info@hackbrightacademy.com>
2014-10-03 15:00:21 -0700

    Created smallest function.

--- skills1.py
+++ skills1.py
@@ -32,8 +32,10 @@ print long_words(word_list)
 
 # Write a function that finds the smallest element in a list of integers and returns it.
 def smallest(number_list):
-    return None
+    number_list.sort()
 
+    return number_list[0]
+print smallest(number_list)
 # Write a function that finds the largest element in a list of integers and returns it.
 def largest(number_list):
     return None
